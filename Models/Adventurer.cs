cbe95c760555b3a0de6302e06d319693a752fb8f
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-15 21:25:11 -0700

    Add files via upload

---
+++ Models/Adventurer.cs
@@ -0,0 +1,7 @@
+namespace BuildRoom.Models {
+    public class Adventurer
+    {
+        public string CharName { get; set; }
+        public string Attack { get; set; }
+    }
+}
--- Models/Room.cs
+++ Models/Room.cs
@@ -4,5 +4,9 @@ namespace BuildRoom.Models
 {
     public class Room {
         public string RoomName {get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public string WallMaterial { get; set; }
+        public string FloorMaterial { get; set; }
     }
 }
