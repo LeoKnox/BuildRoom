4c591e7d64579198f24eeea486dc20e2a20ba5b0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-17 17:26:45 -0700

    Add files via upload

--- Models/Adventurer.cs
+++ Models/Adventurer.cs
@@ -1,10 +1,12 @@
 namespace BuildRoom.Models {
     public class Adventurer
     {
+        public int CharId { get; set; }
         public string CharName { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
         public int Mana { get; set; }
+        public Room Room { get; set; }
 
     public int SwordStrike()
     {
--- Models/Room.cs
+++ Models/Room.cs
@@ -5,13 +5,13 @@ namespace BuildRoom.Models
 {
     public class Room
     {
-        private List<Adventurer> adventurers;
+        public int RoomId { get; set; }
 
         public string RoomName { get; set; }
         public int Width { get; set; }
         public int Length { get; set; }
         public string WallMaterial { get; set; }
         public string FloorMaterial { get; set; }
-        public List<Adventurer> Adventurers { get; set; }
+        public virtual List<Adventurer> Adventurers { get; set; }
     }
 }
